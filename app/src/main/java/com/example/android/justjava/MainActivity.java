8bbc7d3e1a21f394177cdb06b9056823507e8f79
MNada <muhammad.nada@gmail.com>
2018-06-03 15:22:45 +0200

    Changing the display() to displayQuantity() (After 5-2)

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -31,12 +31,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void increment(View view){
         quantity +=1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     public void decrement(View view){
         quantity -=1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
 
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
